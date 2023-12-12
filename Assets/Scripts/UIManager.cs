8b4185efcce7724b5e5026410e52b9386da85452
Eric Brown <eric.brown8787@gmail.com>
2023-12-11 21:15:51 -0500

    Rename scenes to screen names and add navigation methods

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-   public void StartGame()
+
+    public string lastScene; 
+    public void LoadSinglePlayerGame()
     {
-        SceneManager.LoadScene("CoreLoop");
+        lastScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("GameLoop");
     }
-    public void PlayerSelect()
+
+    public void LoadMultiplayerGame()
     {
-        SceneManager.LoadScene("SelectPlayers");
+        lastScene = SceneManager.GetActiveScene().name;
+        Debug.Log("Multiplayer Game not implemented.");
     }
+    public void LoadOptionsScreen()
+    {
+        lastScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(lastScene);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
